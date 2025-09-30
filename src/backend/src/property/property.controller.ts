ae75a70c506548dab41b65175e819de645e8d575
theYassonDC <yassonword@gmail.com>
2025-09-30 18:53:53 -0500

    fix(backend): arreglos del backend

--- src/backend/src/property/property.controller.ts
+++ src/backend/src/property/property.controller.ts
@@ -1,40 +1,41 @@
 import {
-  Body,
-  Controller,
-  Get, HttpCode,
-  Param, ParseIntPipe,
-  Patch,
-  Post,
-} from '@nestjs/common';
-import { CreatePropertyDto } from './dto/createProperty.dto';
-import { ParseIdPipe } from './pipes/parseIdPipes';
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	Param,
+	ParseIntPipe,
+	Patch,
+	Post,
+} from "@nestjs/common";
+import { CreatePropertyDto } from "./dto/createProperty.dto";
+import { ParseIdPipe } from "./pipes/parseIdPipes";
 
-@Controller('property')
+@Controller("property")
 export class PropertyController {
+	@Get()
+	findAll() {
+		return "All Properties";
+	}
 
-  @Get()
-  findAll() {
-    return "All Properties"
-  }
+	@Get(":id")
+	findOne(@Param("id", ParseIntPipe) id: number) {
+		console.log(typeof id);
+		return id;
+	}
 
-  @Get(":id")
-  findOne(@Param("id", ParseIntPipe) id: number) {
-    console.log(typeof id)
-    return id
-  }
+	@Post()
+	// @UsePipes()
+	@HttpCode(202)
+	create(
+		@Body()
+		body: CreatePropertyDto,
+	) {
+		return body;
+	}
 
-  @Post()
-  // @UsePipes()
-  @HttpCode(202)
-  create(@Body()
-  body: CreatePropertyDto) {
-    return body;
-  }
-
-  @Patch(":id")
-  update(
-    @Param("id", ParseIdPipe) id,
-    @Body() body: CreatePropertyDto) {
-    return body
-  }
+	@Patch(":id")
+	update(@Param("id", ParseIdPipe) _id, @Body() body: CreatePropertyDto) {
+		return body;
+	}
 }
